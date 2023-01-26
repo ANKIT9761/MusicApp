00e92c9c0b7edf1fd76c230dcb5c68a215317abf
Ankit Kanyal <ankitsinghkanyal43@gmail.com>
2023-01-26 14:34:15 +0530

    Done with basic scaffolding of the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
